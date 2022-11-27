b996d9d64c9f9c036e467efd094c9e75b9b887df
Adam Sackfield <adamsackfield20@gmail.com>
2022-11-27 23:32:54 +0000

    Add game board, set up game class and methods for buildingGrid, adding snake and moving to the right, with a button for now for

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,65 @@
 console.log('Connected!');
+const game = document.getElementById('game');
+
+class Game {
+	constructor(gridSize) {
+		this.gridSize = gridSize;
+		this.score = 0;
+		this.highScore = 0;
+
+		this.snake = [
+			[8, 8],
+			[8, 7],
+			[8, 6],
+		];
+	}
+
+	buildGrid() {
+		for (let i = 0; i < this.gridSize; i++) {
+			for (let j = 0; j < this.gridSize; j++) {
+				let square = document.createElement('div');
+				square.classList.add('background');
+				square.id = `${i},${j}`;
+
+				game.appendChild(square);
+			}
+		}
+	}
+
+	addSnake() {
+		this.snake.forEach((segment) => {
+			document.getElementById(segment).classList.add('snake-body');
+		});
+	}
+
+	moveSnake(direction) {
+		let head, tail, headSquare, tailSquare;
+
+		if (direction === 'right') {
+			head = [this.snake[0][0], this.snake[0][1] + 1];
+			tail = this.snake.pop();
+			this.snake.unshift(head);
+
+			headSquare = document.getElementById(`${this.snake[0].join(',')}`);
+			tailSquare = document.getElementById(tail.join(','));
+
+			if (headSquare === null) {
+				alert('Game Over');
+				return;
+			}
+
+			headSquare.classList.add('snake-body');
+			tailSquare.classList.remove('snake-body');
+		}
+	}
+}
+
+const newGame = new Game(32);
+
+newGame.buildGrid();
+
+newGame.addSnake();
+
+const rightBtn = document.getElementById('right');
+
+rightBtn.addEventListener('click', () => newGame.moveSnake('right'));
