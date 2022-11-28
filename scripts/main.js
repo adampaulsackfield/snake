e39fb79305c9da18a57fbd3bdfda82c9d5c9a075
Adam Sackfield <adamsackfield20@gmail.com>
2022-11-28 13:59:28 +0000

    Can eat food, but timing is off

--- scripts/main.js
+++ scripts/main.js
@@ -14,6 +14,7 @@ class Game {
 		];
 		this.currentDirection = '';
 		this.speed = 3;
+		this.food = [];
 	}
 
 	buildGrid() {
@@ -33,51 +34,49 @@ class Game {
 			document.getElementById(segment).classList.add('snake-body');
 		});
 
-		this.getScores();
+		// this.getScores();
+		this.addFood();
 	}
 
 	directSnake(direction) {
 		// TODO - Bug where if you click the current direction again it begins to speed up.
+		const isFood = this.isFood(this.snake[0]);
 
 		if (direction === 'right' && this.currentDirection !== 'left') {
 			const row = this.snake[0][0];
 			const col = this.snake[0][1] + 1;
 
-			this.makeMove(row, col, 'right');
+			this.makeMove(row, col, 'right', isFood);
 		}
 
 		if (direction === 'left' && this.currentDirection !== 'right') {
 			const row = this.snake[0][0];
 			const col = this.snake[0][1] - 1;
 
-			this.makeMove(row, col, 'left');
+			this.makeMove(row, col, 'left', isFood);
 		}
 
 		if (direction === 'up' && this.currentDirection !== 'down') {
 			const row = this.snake[0][0] - 1;
 			const col = this.snake[0][1];
 
-			this.makeMove(row, col, 'up');
+			this.makeMove(row, col, 'up', isFood);
 		}
 
 		if (direction === 'down' && this.currentDirection !== 'up') {
 			const row = this.snake[0][0] + 1;
 			const col = this.snake[0][1];
 
-			this.makeMove(row, col, 'down');
+			this.makeMove(row, col, 'down', isFood);
 		}
 
 		return;
 	}
 
-	makeMove(row, col, direction) {
+	makeMove(row, col, direction, eating) {
 		let head = [row, col];
-		let tail = this.snake.pop();
-		let headSquare, tailSquare;
+		let headSquare, tailSquare, foodSquare;
 		this.snake.unshift(head);
-
-		headSquare = document.getElementById(`${this.snake[0].join(',')}`);
-		tailSquare = document.getElementById(tail.join(','));
 
 		if (headSquare === null) {
 			const info = document.getElementById('info');
@@ -85,8 +84,19 @@ class Game {
 			return;
 		}
 
+		headSquare = document.getElementById(`${this.snake[0].join(',')}`);
+		foodSquare = document.getElementById(`${this.food.join(',')}`);
+
 		headSquare.classList.add('snake-body');
-		tailSquare.classList.remove('snake-body');
+
+		if (eating) {
+			foodSquare.classList.remove('food');
+			this.addFood();
+		} else {
+			let tail = this.snake.pop();
+			tailSquare = document.getElementById(tail.join(','));
+			tailSquare.classList.remove('snake-body');
+		}
 
 		this.currentDirection = direction;
 
@@ -94,6 +104,29 @@ class Game {
 			if (this.currentDirection !== direction) return;
 			else this.directSnake(this.currentDirection);
 		}, 1200 / this.speed);
+	}
+
+	addFood() {
+		const randomLocation = `${Math.floor(Math.random() * 17)},${Math.floor(
+			Math.random() * 17
+		)}`;
+
+		this.snake.forEach((segment) => {
+			if (segment.join(',') === randomLocation) {
+				this.addFood();
+			} else {
+				this.food = randomLocation.split(',');
+				document.getElementById(randomLocation).classList.add('food');
+			}
+		});
+	}
+
+	isFood(head) {
+		if (this.food.join(',') === head.join(',')) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	getScores() {
