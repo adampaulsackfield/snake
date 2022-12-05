36a7d8c6ba05314971e68a6dbb19a4f5da1565e6
Adam Sackfield <adam@adamsackfield.uk>
2022-12-05 23:16:59 +0000

    remove old api folder

--- api/routes/index.js
+++
@@ -1,7 +0,0 @@
-const { getScores, addScore } = require('../controllers/index');
-
-const router = require('express').Router();
-
-router.route('/scores').get(getScores).post(addScore);
-
-module.exports = router;
